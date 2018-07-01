ca45f72127b60af6dfbb8449d6563522498fbac2
Alvaro Enrique <31602481+alvaroenrique@users.noreply.github.com>
2018-06-30 21:48:35 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Ejemplo1 class="var"></Ejemplo1>
+  </div>
+</template>
+
+<script>
+import Ejemplo1 from "./components/Ejemplo1";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "App",
+  components: { Ejemplo1 }
+};
+</script>
+
+<style>
+.var {
+}
+#app {
+  text-align: center;
+}
+</style>
---
+++ src/components/Ejemplo1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h2>Ejemplo práctico 1</h2>
+    <hr>
+
+    <h4>var1 = {{var1}}</h4> 
+    <h4>var2 = {{var2}}</h4> 
+    <h4>var3 (A x B) = {{var1 * var2}}</h4>
+
+    <div>
+      Cambiar el valor de Var1: <input type="text" v-model="var1">
+    </div>
+    <div>
+      Cambiar el valor de Var2: <input type="text" v-model="var2">
+    </div>
+  </div>
+</template>
+
+<script>
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+
+export default {
+  name: "Ejemplo1",
+  data() {
+    return {
+      var1: 3,
+      var2: 10
+    };
+  },
+  methods: {},
+  created() {}
+};
+</script>
+
+<style>
+input {
+  text-align: center;
+  width: 50px;
+}
+</style>
